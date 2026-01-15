2d4a08aad57d3114e82343c872650803a37686a4
arthur-georget <arthur.georget@laplateforme.io>
2026-01-15 12:10:21 +0100

    feat(calc): unlimited parenthesis, multiplication and division priority support. every cases proof for now.

--- modules/calcul_orchestrator.py
+++ modules/calcul_orchestrator.py
@@ -1,20 +1,30 @@
+from calculator import calculator
 def calcul_orchestrator(elements):
+    """
+    calcul_orchestrator(tuple) -> float
+    
+    """
     elements_levels = []
     for i in range(len(elements)): # elements_levels init
         elements_levels.append((elements[i],0))
     
     parenthesis = False # parenthesis init
+    # check if there is a parenthesis in the elements
     for i in range(len(elements)):
         if elements[i] == "(":
             parenthesis = True
-            opener_index = i
             break
-    # while we find parenthesis in elements_levels we recursively replace elements_levels 
-    # between parenthesis content with calcul_orchestrator call
+    """ while we find parenthesis in elements_levels we put 
+    what's between parenthesis in sub_calculation list and replace
+    what's between parenthesis with calcul_orchestrator(sub_calculation) output """
     while parenthesis:
         level = 0
         sub_calculation = []
-        #Scan elements increase level when finding "(" and decrease when finding ")"
+        for i in range(len(elements_levels)):
+            if elements_levels[i][0] == "(":
+                opener_index = i
+                break
+        # Scan elements increase level when finding "(" and decrease when finding ")"
         for i in range(len(elements_levels)):
             if elements_levels[i][0] == "(":
                 level += 1
@@ -25,39 +35,61 @@ def calcul_orchestrator(elements):
             if i > opener_index and elements_levels[i][1] < 1:
                 closer_index = i
                 break
-        for i in range(opener_index+1,closer_index): #Opener index +1 to skip the opening parenthesis
+        for i in range(opener_index+1,closer_index): # opener_index +1 to skip the opening parenthesis
             sub_calculation.append(elements_levels[i][0])
         print(f"elements_levels_before_replacement: {elements_levels}")
         for i in range(closer_index,opener_index,-1):
-            elements_levels.pop(i) #Remove sub_calculation elements from elements_levels
+            elements_levels.pop(i) # Remove sub_calculation elements from elements_levels
         
-        elements_levels[opener_index] = (calcul_orchestrator(tuple(sub_calculation)),0) #Replace opener_index value because it's "("
+        elements_levels[opener_index] = (calcul_orchestrator(tuple(sub_calculation)),0) # Replace opener_index value because it's "("
         
-            #print(f"i: {i}")
+        """  #print(f"i: {i}")
         print(f"elements_levels: {elements_levels}")
         print(f"opener_index: {opener_index}")
         print(f"closer_index: {closer_index}")
-        print(f"sub_calculation: {sub_calculation}")
+        print(f"sub_calculation: {sub_calculation}") """
         
+        # Parenthesis presence check
         for i in range(len(elements_levels)):
             if elements_levels[i][0] == "(":
                 parenthesis = True
                 break
             else:
                 parenthesis = False
-    for i in range(len(elements)):
-        match elements[i]:
-            case "+":
-                
-            case "-":
-            case "*":
-            case "/":
-# ((3+6*7)*57)         
-my_elements = ("(","(",3,"+",6,"*",7,")","*",57,")")
+
+    # Orchestring operations when no parenthesis is found
+    elements_levels_remaining = True
+    i = 0
+    while elements_levels_remaining: # First loop for multiplication and divisions
+        if elements_levels[i][0] == "*" or elements_levels[i][0] == "/":
+            elements_levels[i-1] = (calculator(elements_levels[i-1][0],elements_levels[i+1][0],elements_levels[i][0]),0)
+            elements_levels.pop(i+1)
+            elements_levels.pop(i)
+            i-=1
+        else:
+            i+=1
+            if len(elements_levels) <= i:
+                elements_levels_remaining = False
+    elements_levels_remaining = True
+    i = 0
+    while elements_levels_remaining: # Second loop for additions and substractions
+        if elements_levels[i][0] == "+" or elements_levels[i][0] == "-":
+            elements_levels[i-1] = (calculator(elements_levels[i-1][0],elements_levels[i+1][0],elements_levels[i][0]),0)
+            elements_levels.pop(i+1)
+            elements_levels.pop(i)
+            i-=1
+        else:
+            i+=1
+            if len(elements_levels) <= i:
+                elements_levels_remaining = False
+    return elements_levels[0][0] # Return what's left at the end
+
+# ((3+6*7)*57)        
+#my_elements = ("(","(",3,"+",6,"*",7,")","*",57,")")
 # 6*8/((3-2)+4)
-my_elements2 = (6,"*",8,"/","(","(",3,"-",2,")","+",4,")")
+#my_elements2 = (6,"*",8,"/","(","(",3,"-",2,")","+",4,")")
 # (6+8)/(3-2)+4
-my_elements3 = ("(",6,"+",8,")","/","(",3,"-",2,")","+",4)
-calcul_orchestrator(my_elements)
-calcul_orchestrator(my_elements2)
-calcul_orchestrator(my_elements3)
+#my_elements3 = ("(",6,"+",8,")","/","(",3,"-",2,")","+",4)
+#print(calcul_orchestrator(my_elements))
+#print(calcul_orchestrator(my_elements2))
+#print(calcul_orchestrator(my_elements3))
--- modules/calculator.py
+++ modules/calculator.py
@@ -14,4 +14,7 @@ def calculator(number_a, number_b, operator):
         return result 
 
     except :
-        return Error
+        print("Oops! Something went wrong in calculator function..., maybe you gave me something I can't handle.")
+        print(f"number_a: {number_a}")
+        print(f"number_b: {number_b}")
+        print(f"operator: {operator}")
